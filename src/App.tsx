92719ee7ec11de2ebbbce5a71afd73e7c5f43f4e
UstmanAbdurahman <72262190+UstmanAbdurahman@users.noreply.github.com>
2021-07-03 20:01:58 +0700

    ✏🔧 Made under construction page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import BuildRoundedIcon from '@material-ui/icons/BuildRounded';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <BuildRoundedIcon
+          className="App-logo"
+          style={{
+            fontSize: 200
+          }}
+        />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          website under development, lol sorry
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <b>
+            wait
+          </b>
+        </p>
       </header>
     </div>
   );
